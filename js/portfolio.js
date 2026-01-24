153a847df4e3802ff028910f000376db1e22682f
Quinn Olney <quinnolney123@gmail.com>
2026-01-23 21:13:42 -0700

    even more portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,18 +2,26 @@ const onButton = document.getElementById("on-btn");
 const tv = document.getElementById("tv");
 const tvDisplay = document.getElementById("tv-display");
 
-const projectImage = document.getElementById("project-img");
-const projectTitle = document.getElementById("project-title");
+const tvTitle = document.querySelector('#tv .project-title');
+const tvImage = document.querySelector('#tv .project-img');
+
+const modalTitle = document.querySelector('#details-modal .project-title');
+
 
 const playButton = document.getElementById('play');
 const skipForward = document.getElementById('forward');
 const skipBackward = document.getElementById('prev');
 
+const projectDetails = document.getElementById('details-modal');
+const projectDescription = document.getElementById('project-details');
+
+const modalImage = document.getElementById('modal-img');
+
 const projects = [
     {
         image: '../assets/portfolio-assets/dupify.png',
         title: 'Dupify',
-        description: '',
+        description: 'hello',
     },
     {
         image: '',
@@ -50,13 +58,45 @@ onButton.addEventListener('click', () => {
 
 let projectIndex = 0;
 
+skipForward.addEventListener('click', () => {
+
+    if (projectIndex < projects.length && (tv.classList.contains('on')))
+    {
+        projectIndex += 1;
+    }
+
+    UpdateProject();
+
+});
+
+skipBackward.addEventListener('click', () => {
+
+    if (projectIndex > 0 && (tv.classList.contains('on')))
+    {
+        projectIndex -= 1;
+    }
+
+    UpdateProject();
+});
+
+playButton.addEventListener('click', () => {
+
+    if (tv.classList.contains('on'))
+    {
+        projectDetails.classList.toggle('show');
+    }
+});
 
 
 
 function UpdateProject() {
     const project = projects[projectIndex];
-    projectImage.src = project.image;
-    projectTitle.innerText = project.title;
+
+    tvTitle.innerText = project.title;
+    tvImage.src = project.image;
+
+    modalTitle.innerText = project.title;
+    modalImage.src = project.image;
     
 }
 
